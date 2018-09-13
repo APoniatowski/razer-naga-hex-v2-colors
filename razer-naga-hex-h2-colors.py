7d96463bf01b79c63f827c38d87be6e742823370
Adam Poniatowski <adam.poniatowski@outlook.com>
2018-09-13 12:38:17 +0200

    added comments to mouse locations, also to make more sense of it

--- razer-naga-hex-h2-colors.py
+++ razer-naga-hex-h2-colors.py
@@ -8,9 +8,9 @@ for device in a.devices:
     device.poll_rate = 1000
     if device.type == "mouse":
         device.brightness = 90
-        device.fx.static(255, 0, 0)
-        device.fx.misc.logo.breath_single(0,255,0)
-        device.fx.misc.scroll_wheel.static(0,0,255) 
+        device.fx.static(255, 0, 0)  # hex thumb keys colors
+        device.fx.misc.logo.breath_single(0,255,0)  # The logo's colors
+        device.fx.misc.scroll_wheel.static(0,0,255)  # The scroll wheel's colors
 
 # To change effects, play around with the numbers and effects
 # There are options like .static(x,x,x) , .breath_dual( not sure how to use this, as the obvious way does not work ), .wave(), etc  
